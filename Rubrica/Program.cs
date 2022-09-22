57eded8fa3d2c3864e73f68ce7bec2e32a8ce72a
Fabio Iotti <fabio.iotti@alad.cloud>
2022-09-22 14:15:12 +0200

    Aggiunta commenti

--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -5,8 +5,13 @@ using System.Text.Json.Serialization;
 // serialize / serializza   = converti da Classe C# a stringa di testo JSON
 // deserialize/deserializza = converti da stringa di testo JSON a Classe C#
 
+// Nome del file contentente la rubrica.
+// Ogni riga di questo file è un documento JSON.
 const string FileName = "rubrica.json";
 
+// funzione che permette di chiedere un parametro.
+// Se il paramentro "index" è stato indicato da linea di comando usa quello,
+// altrimenti mostra un "messaggio" all'utente chiedendo il parametro.
 string ChiediParametro(int index, string messaggio)
 {
     if (args.Length > index)
@@ -24,7 +29,8 @@ string ChiediParametro(int index, string messaggio)
     return input;
 }
 
-string operazione = ChiediParametro(0, "Operazioni disponibili: lista, cerca, nuovo\nCosa vuoi fare? ");
+// Chiede all'utente che operazione vuole esseguire sulla rubrica.
+string operazione = ChiediParametro(0, "Operazioni disponibili: lista, cerca, nuovo, cancella\nCosa vuoi fare? ");
 switch (operazione)
 {
     case "lista":
@@ -53,6 +59,7 @@ switch (operazione)
         throw new Exception("Comando non valido.");
 }
 
+// Deserializza un contatto dal JSON.
 Contatto DeserializzaContatto(string json)
 {
     Contatto? contatto = JsonSerializer.Deserialize<Contatto>(json);
@@ -64,12 +71,14 @@ Contatto DeserializzaContatto(string json)
     return contatto;
 }
 
+// Legge i contatti dal file indicato.
 IEnumerable<Contatto> LeggiContatti(string fileName)
 {
     return File.ReadLines(fileName)
         .Select(DeserializzaContatto);
 }
 
+// Stampa una lista di contatti.
 void StampaTutti(IEnumerable<Contatto> contatti)
 {
     foreach (Contatto contatto in contatti)
@@ -78,6 +87,7 @@ void StampaTutti(IEnumerable<Contatto> contatti)
     }
 }
 
+// Stampa da una lista di contatti quelli che hanno "query" nel nome, cognome o numero.
 void StampaFiltrati(IEnumerable<Contatto> contatti, string query)
 {
     IEnumerable<Contatto> filtrati = contatti
@@ -86,6 +96,7 @@ void StampaFiltrati(IEnumerable<Contatto> contatti, string query)
     StampaTutti(filtrati);
 }
 
+// Aggiunge un contatto come ultima riga del file.
 void AggiungiContatto(Contatto contatto, string fileName)
 {
     string json = JsonSerializer.Serialize(contatto);
@@ -95,38 +106,59 @@ void AggiungiContatto(Contatto contatto, string fileName)
     Console.WriteLine("Contatto aggiunto!");
 }
 
+// Cancella dal file il contatto che ha "vittima" nel nome, cognome o numero.
 void CancellaContatto(string vittima, string fileName)
 {
+    // leggiamo tutte le righe del file, e carichiamole in un array
     string[] linee = File.ReadAllLines(fileName);
-
+    
+    // creiamo una lista vuota delle righe che intendiamo mantenere (non cancellare)
     List<string> mantenere = new List<string>();
 
+    // iteriamo ciascuna delle righe lette dal file
     foreach (string linea in linee)
     {
+        // converte la riga da JSON a classe C# "Contatto"
         Contatto c = DeserializzaContatto(linea);
 
+        // controlliamo se la striga "vittima" appare nel nome, cognome o numero del contatto
         if (c.Nome.Contains(vittima) || c.Cognome.Contains(vittima) || c.Numero.Contains(vittima))
-            continue;
+            continue;  // interrompe l'iterazione del ciclo, ovvero non esegue il codice seguente nel ciclo
 
+        // se "vittima" non appare nel nome, cognome o numero del contatto, aggiungiamo alla lista di contatti da mantenere
         mantenere.Add(linea);
     }
 
+    // tutte le linee eccetto quelle da mantenere, ovvero solo le linee da mantenere
+    // per ciascuna delle linee trovate, chiamiamo "DeserializzaContatto" per convertire da JSON a classe C# "Contatto"
     IEnumerable<Contatto> contattiCancellati = linee.Except(mantenere).Select(DeserializzaContatto);
 
+    // se c'è più di un contatto che risponde alla query "vittima"...
     if (contattiCancellati.Count() > 1)
     {
+        // ...diciamo che la query è ambigua, non cancelliamo nulla,
+        // e facciamo vedere tutti i contatti che rispondono alla query
         Console.WriteLine("Nome ambiguo:");
         StampaTutti(contattiCancellati);
     }
+
+    // se ce n'è esattamente uno solo...
     else if (contattiCancellati.Count() == 1)
     {
+        // ...leggiamo il primo, ovvero l'unico
         Contatto c2 = contattiCancellati.First();
 
+        // scriviamo nel file tutte le righe da mantenere
         File.WriteAllLines(fileName, mantenere);
+
+        // diciamo qual'era il contatto cancellato
         Console.WriteLine($"Cancellato {c2.Nome} {c2.Cognome} ({c2.Numero})");
     }
+
+    // se non ce ne sono...
     else
     {
+        // diciamo che l'operazione non è valida
         Console.WriteLine($"{vittima} non trovato.");
     }
 }
