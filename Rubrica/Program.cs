0d181aac9b5b1e5b5d178c1d33e101501721ad73
Fabio Iotti <fabio.iotti@alad.cloud>
2022-09-22 10:48:55 +0200

    Esercizio 1

--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+IEnumerable<string> linee = File.ReadLines("rubrica.json");
+
+string primaLinea = linee.First();
+Contatto? contatto = JsonSerializer.Deserialize<Contatto>(primaLinea);
+if (contatto == null)
+{
+    throw new Exception("JSON non valido.");
+}
+
+Console.WriteLine($"{contatto.Nome} {contatto.Cognome}: {contatto.Numero}");
+
+class Contatto
+{
+    public string Nome { get; set; } = "";
+    public string Cognome { get; set; } = "";
+    public string Numero { get; set; } = "";
+}
